44c128106a9f164fe7e77a5d2091d5ed24f0c8ab
Will_Zhu <will_zyq@qq.com>
2020-06-17 00:52:45 +1000

    solve p6

--- p6.hs
+++ p6.hs
@@ -1,36 +1,11 @@
 suffix :: Eq a => [a] -> [a] -> Maybe [a]
-suffix [] ys = Just ys
-suffix lx@(x:xs) ly@(y:ys) = suffixStart lx ly index 
+suffix xs ys
+    | xs_length > ys_length = Nothing
+    | all (==False) xs_check_list = Nothing
+    | otherwise = Just $ drop ys_index ys
     where
-        index = findIndex x ly
-
-suffixStart :: Eq a => [a] -> [a] -> Maybe Int -> Maybe [a]
-suffixStart _ _ Nothing = Nothing
-suffixStart lx@(x:xs) ly@(y:ys) (Just n) = suffixWork lx ly_start
-    where ly_start = drop n ly
-
-suffixWork :: Eq a => [a] -> [a] -> Maybe [a]
-suffixWork (x:xs) (y:ys)
-    | x /= y = Nothing
-    | otherwise = suffixWork xs ys
-suffixWork [] ys = Just ys
-
-findIndex :: Eq a => a -> [a] -> Maybe Int
-findIndex x l@(y:ys) =
-    if elem x l then
-        if x == y then
-            Just 0
-        else
-            do
-                n <- findIndex x ys
-                return (n + 1)
-    else
-        Nothing
-
-findIndex :: Eq a => [a] -> [a] -> Maybe Int
-findIndex [] ys = Just 0
-findIndex (x:xs) (y:ys)
-    | x /= y = do
-        n1 <- findIndex (x:xs) ys
-        return (n1 + 1)
-    | otherwise = 
+        xs_length = length xs
+        ys_length = length ys
+        l = [0..(ys_length - xs_length)]
+        xs_check_list = map (\x -> all (==True) (zipWith (==) (drop x ys) xs)) l
+        ys_index = xs_length + length (takeWhile (/=True) xs_check_list)
